9c7b0e65d7500e5dfda62ecfe58b312fc3fdf062
Yong Ju Kwak <amese1225@gmail.com>
2020-10-05 20:59:04 +0900

    #1.3 Functions part One

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func multiply(a int, b int) int {
+	return a * b
+}
+
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+func repeatString(name ...string) {
+	fmt.Println(name)
+}
 
 func main() {
-	var name string = "test"
-	testing := "same"
-	name = "change"
-	fmt.Println(name)
-	fmt.Println(testing)
+	totalLength, _ := lenAndUpper("nico")
+	fmt.Println(totalLength)
+
+	repeatString("A", "BB", "CCC")
 }
