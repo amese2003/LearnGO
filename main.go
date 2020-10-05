20f779fdf7d93ce4fa7033c323f788d1cf6a5411
Yong Ju Kwak <amese1225@gmail.com>
2020-10-06 07:16:01 +0900

    #1.6 If with a Twist

--- main.go
+++ main.go
@@ -36,6 +36,14 @@ func superAdd(numbers ...int) int {
 	return sum
 }
 
+func canDrink(age int) bool {
+	if koreanAge := age + 2; koreanAge < 18 {
+		return false
+	}
+
+	return true
+}
+
 func main() {
 	total := superAdd(1, 2, 3, 4, 5, 6, 7)
 
