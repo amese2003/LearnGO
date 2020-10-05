dcb638899828a2c437395c6dbb33fd2a5947ffd1
Yong Ju Kwak <amese1225@gmail.com>
2020-10-06 07:26:29 +0900

    #1.10 Maps

--- main.go
+++ main.go
@@ -2,67 +2,13 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-func multiply(a int, b int) int {
-	return a * b
-}
-
-func lenAndUpper(name string) (length int, uppercase string) {
-	defer fmt.Println("I'm done")
-
-	length = len(name)
-	uppercase = strings.ToUpper(name)
-	return
-}
-
-func repeatString(name ...string) {
-	fmt.Println(name)
-}
-
-func superAdd(numbers ...int) int {
-	var sum int = 0
-
-	// for index, number := range numbers {
-	// 	fmt.Println(index, number)
-	// 	sum += number
-	// }
-
-	for i := 0; i < len(numbers); i++ {
-		sum += numbers[i]
-	}
-
-	return sum
-}
-
-func canDrink(age int) bool {
-	// if koreanAge := age + 2; koreanAge < 18 {
-	// 	return false
-	// }
-
-	switch age {
-	case 10:
-		return false
-	case 18:
-		return true
-	}
-
-	return true
-}
-
 func main() {
-	a := 2
-	b := &a
+	names := map[string]string{"name": "asd", "age": "12"}
+	//fmt.Println(names)
 
-	*b = 5
-
-	fmt.Println(a, *b)
-
-	names := []string{"as", "df", "ad"}
-	names = append(names, "blabla")
-
-	//names[3] = "temp"
-
-	fmt.Println(names)
+	for key, _ := range names {
+		fmt.Println(key)
+	}
 }
