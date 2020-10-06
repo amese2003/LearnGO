33089e60ecae9c725295f9d21f29539ac9dcfaf8
Yong Ju Kwak <amese1225@gmail.com>
2020-10-06 21:56:59 +0900

    #3.7 FAST URLChecker

--- main.go
+++ main.go
@@ -6,14 +6,14 @@ import (
 	"time"
 )
 
-type result struct {
+type requestResult struct {
 	url    string
 	status string
 }
 
 func main() {
-	//results := make(map[string]string)
-	c := make(chan result)
+	results := make(map[string]string)
+	c := make(chan requestResult)
 	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
@@ -28,11 +28,19 @@ func main() {
 	for _, url := range urls {
 		go hitURL(url, c)
 	}
+
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
+	}
+
+	for url, status := range results {
+		fmt.Println(url, status)
+	}
+
 }
 
-func hitURL(url string, c chan<- result) {
-	fmt.Println("checking:", url)
-
+func hitURL(url string, c chan<- requestResult) {
 	resp, err := http.Get(url)
 	status := "OK"
 
@@ -40,7 +48,7 @@ func hitURL(url string, c chan<- result) {
 		status = "FAILED"
 	}
 
-	c <- result{url: url, status: status}
+	c <- requestResult{url: url, status: status}
 }
 
 func isCheck(person string, c chan string) {
