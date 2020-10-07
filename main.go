a65110a74baccfdb253aa5bd840228f7979157cd
Yong Ju Kwak <amese1225@gmail.com>
2020-10-07 17:15:22 +0900

    #4.1 getPages part Two

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -11,11 +12,20 @@ import (
 var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
 func main() {
-	getPages()
+	totalPages := getPages()
 
+	for i := 0; i < totalPages; i++ {
+		getPage(i)
+	}
+}
+
+func getPage(page int) {
+	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
+	fmt.Println("Requesting", pageURL)
 }
 
 func getPages() int {
+	pages := 0
 	res, err := http.Get(baseURL)
 	checkErr(err)
 	checkCode(res)
@@ -24,11 +34,11 @@ func getPages() int {
 
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkErr(err)
-	//doc.Find(".pagination")
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		pages = s.Find("a").Length()
+	})
 
-	fmt.Println(doc)
-
-	return 0
+	return pages
 }
 
 func checkErr(err error) {
